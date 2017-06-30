cd8040c17574f032a77b2fa32bd2b665a0b3384c
huangdali <137722697@qq.com>
2017-06-30 11:32:49 +0800

    2x版

--- soundwave/src/main/java/com/jwkj/soundwave/SoundWaveSender.java
+++ soundwave/src/main/java/com/jwkj/soundwave/SoundWaveSender.java
@@ -2,9 +2,9 @@ package com.jwkj.soundwave;
 
 import android.content.Context;
 
-import com.hdl.udpsenderlib.UDPReceiver;
 import com.hdl.udpsenderlib.UDPResult;
 import com.hdl.udpsenderlib.UDPResultCallback;
+import com.hdl.udpsenderlib.UDPSender;
 import com.lsemtmf.genersdk.tools.emtmf.EMTMFSDK;
 import com.lsemtmf.genersdk.tools.emtmf.EMTMFSDKListener;
 
@@ -16,6 +16,7 @@ public class SoundWaveSender {
     private Context mContext;
     private int port = 9988;
     private ResultCallback callback;
+    private boolean isCanReceive = true;//时候可以接收
 
     private SoundWaveSender() {
     }
@@ -29,6 +30,7 @@ public class SoundWaveSender {
 
     public SoundWaveSender with(Context context) {
         this.mContext = context;
+        checkContextIsNull();
         EMTMFSDK.getInstance(context).setListener(emtmfsdkListener);
         return this;
     }
@@ -44,36 +46,79 @@ public class SoundWaveSender {
         return this;
     }
 
-    public void send(final ResultCallback callback) {
-        this.callback = callback;
-
+    public void send(ResultCallback callback) {
+        checkContextIsNull();
+        if (this.callback == null) {
+            this.callback = callback;
+        }
         EMTMFSDK.getInstance(mContext).sendWifiSet(mContext, ssid, pwd);
-        UDPReceiver.getInstance().with(mContext).setPort(port).receive(new UDPResultCallback() {
-            @Override
-            public void onStart() {
-                callback.onStart();
-            }
-
-            @Override
-            public void onNext(UDPResult udpResult) {
-                callback.onNext(udpResult);
-            }
-
-            @Override
-            public void onCompleted() {
-                super.onCompleted();
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                callback.onError(throwable);
-            }
-        });
+        if (isCanReceive) {
+            isCanReceive = false;
+            startReceive();
+        }
     }
+
+    /**
+     * 开始接收数据，单次任务只能调用一次
+     */
+    private void startReceive() {
+        checkContextIsNull();
+        UDPSender.getInstance()
+                .setInstructions(new byte[]{0, 1, 1, 1})
+                .setReceiveTimeOut(10 * 60 * 1000)
+                .setLocalReceivePort(port)
+                .setTargetPort(port)
+                .start(new UDPResultCallback() {
+                    @Override
+                    public void onNext(UDPResult result) {
+                        if (result.getResultData()[0] == 1) {
+                            callback.onNext(result);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        super.onError(throwable);
+                        callback.onError(throwable);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        callback.onCompleted();
+                    }
+                });
+    }
+
+    /**
+     * 停止接收数据
+     */
+    private void stopReceive() {
+        checkContextIsNull();
+        isCanReceive = true;//停止任务了要复位
+        if (UDPSender.getInstance() != null) {
+            UDPSender.getInstance().stop();
+        }
+    }
+
+    /**
+     * 检测上下文对象是否为空
+     */
+    private void checkContextIsNull() {
+        if (mContext == null) {
+            throw new NullPointerException("context is null,please call SoundWaveSender.getInstance().with(context).***");
+        }
+    }
+
+    /**
+     * 停止发送声波
+     *
+     * @return
+     */
     public SoundWaveSender stopSend() {
-        EMTMFSDK.getInstance(mContext).stopSend();
-//        UDPSender.getInstance().stop();
-        UDPReceiver.getInstance().with(mContext).stopReceive();
+        if (EMTMFSDK.getInstance(mContext) != null) {
+            EMTMFSDK.getInstance(mContext).stopSend();
+        }
+        stopReceive();
         return this;
     }
 
